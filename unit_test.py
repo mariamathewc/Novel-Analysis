93ed5ce5843e146653fc870601c9fcda378795fd
Maria Mathew <mariamathewc@gmail.com>
2021-10-03 21:41:20 -0700

    Create unit_test.py

---
+++ unit_test.py
@@ -0,0 +1,21 @@
+# Python program to explain os.getenv() method 
+    
+  
+# Get the value of 'HOME'
+# environment variable
+# Python program to explain os.getenv() method 
+    
+# importing os module 
+import os
+ 
+ 
+# Get the value of 'HOME'
+# environment variable
+key = 'HOME'
+value = os.getenv("test_key)
+print("Debug env", value)
+      
+for k, v in sorted(os.environ.items()):
+    print(k+':', v)
+print('\n')
+
